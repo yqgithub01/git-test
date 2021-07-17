d3c296a34125edf4da23634fb3a3f96824d13837
yqgithub01 <zyqitnet@outlook.com>
2021-07-17 23:01:35 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,4 +6,8 @@ package com.atguigu.git;
  * @Date: 2021/07/17
  */
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+    }
 }
